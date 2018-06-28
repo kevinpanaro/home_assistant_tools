a133fa9fcdc52cd65c0e1686f2f7de984eb81e71
kevinpanaro <panaro.kevin@gmail.com>
2018-06-28 13:52:26 -0600

    pass: change init and reorganize class functions

--- pass/ha_pass.py
+++ pass/ha_pass.py
@@ -5,18 +5,22 @@ from os.path import isfile, join
 
 class HassPass:
 
-    def __init__(self):
-        self.pass_home_dir = "/Users/kevinpanaro/.password-store"
-        self.homeassistant_dir = "homeassistant"
-        self.hasspass_path = join(self.pass_home_dir, self.homeassistant_dir)
-        self.binary='/usr/local/bin/gpg'
-        self.homedir='/Users/kevinpanaro/.gnupg'
-        self.gpg_encoding = 'utf-8'
+    def __init__(self, 
+                 pass_home_dir = "/Users/kevinpanaro/.password-store",
+                 hass_dir = "homeassistant", binary = "/usr/local/bin/gpg",
+                 home_dir = "/Users/kevinpanaro/.gnupg", gpg_encoding = "utf-8",
+                 save_file = "secrets.yaml"):
+        self.pass_home_dir = pass_home_dir
+        self.hass_dir = hass_dir
+        self.hasspass_path = join(self.pass_home_dir, self.hass_dir)
+        self.binary = binary
+        self.homedir = home_dir
+        self.gpg_encoding = gpg_encoding
         self.hasspass_dict = {}
+        self.save_file = save_file
+
         self._setup_gpg()
-        self.hasspass_files = self._get_hasspass_files()
-        self._form_pass_dict()
-        self._make_yaml()
+        self.make_yaml()
 
     def _setup_gpg(self):
         '''sets up gpg'''
@@ -26,9 +30,11 @@ class HassPass:
     def _get_hasspass_files(self):
         '''return list of hass files''' 
         files = [file for file in listdir(self.hasspass_path) if isfile(join(self.hasspass_path, file))]
-        return files
+        self.hasspass_files = files
 
     def _form_pass_dict(self):
+        '''makes pass dict'''
+        self._get_hasspass_files()
         for file in self.hasspass_files:
             current_file = file.split(".")[0]
             file = join(self.hasspass_path, file)
@@ -37,7 +43,9 @@ class HassPass:
 
             self.hasspass_dict[current_file] = str(data).strip()
         
-    def _make_yaml(self):
+    def make_yaml(self):
+        '''creates a yaml, unfinished'''
+        self._form_pass_dict()
         alphabetical = sorted(self.hasspass_dict.keys())
         for component in alphabetical:
             print(component + ": " + self.hasspass_dict[component])
